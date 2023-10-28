9036ae1e1d055380d37b313c1e70aff09b1865f3
akiva_kraus <akiva1132@gmail.com>
2023-10-29 01:25:08 +0300

    fix bug of fetch

--- dist/PayPal/orderVerification.js
+++ dist/PayPal/orderVerification.js
@@ -4,29 +4,27 @@ exports.orderVerification = void 0;
 const clientId = "AWuutuwcDjV1bKKKixdtA9LkXEw8tSNoUjQXpOBIJlqutaGdLakuH-Q0_HXQQuRbegsOrE1ae78yK8Ik";
 const clientSecret = "EKcW6HHiLmaKU0LKX2Kk7OE27tHJDjS3LeTGeKiPG1eUL7F3R5BqD08Y0aizzDm5QHz5wqU-hKK3bwIv";
 const authToken = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+const request = require('superagent');
 const headers = {
     "Content-Type": "application/json",
     Authorization: `Basic ${authToken}`,
 };
 const orderVerification = async (orderId) => {
     try {
-        const response = await fetch(`https://api.paypal.com/v2/checkout/orders/${orderId}`, {
-            method: "GET",
-            headers,
-        });
-        const data = await response.json();
+        const response = await request.get(`https://api.paypal.com/v2/checkout/orders/${orderId}`)
+            .set(headers);
+        const data = response.body;
         if (response.ok) {
             console.log(data);
             return data;
         }
         else {
             console.log(data);
-            throw new error(data);
+            throw new Error(data);
         }
     }
     catch (error) {
-        console.log(error);
-        throw error(error.message);
+        throw new Error(error.message);
     }
 };
 exports.orderVerification = orderVerification;
