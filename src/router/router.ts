aef2b5997cdd23a6e41b7d7a60a4dd56ec7e957b
akiva_kraus <akiva1132@gmail.com>
2023-10-30 22:29:04 +0200

    auth

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 import usersRoutes from "../users/routes/usersRoutes";
 import getAnswerRoutes from "../open-ai-api/getAnswerRoutes"
-
+import { auth } from "../auth/providers/jwt";
 router.use("/api", usersRoutes);
-router.use("/api/get-answer/", getAnswerRoutes);
+router.use("/api/get-answer/",auth, getAnswerRoutes);
 router.use(express.static("../../public"));
 
 router.use("*", (req: Request, res: Response) =>
